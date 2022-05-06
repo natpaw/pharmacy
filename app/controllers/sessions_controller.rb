25aee2dcb3c1666f7c5e75eede7cf6fd3cf38a69
natpaw <msnatpaw@gmail.com>
2022-05-06 22:18:40 +0300

    session deleted

--- app/controllers/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class SessionController < ActionController
-	def create
-		session[:current_order_id] = @order.id
-	end	
-	def index
-		current_order = Order.find_by_id(session[:current_order_id])
-	end
-end
