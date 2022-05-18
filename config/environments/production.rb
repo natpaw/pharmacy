8cc458fb5dc3c7b23576fbacd062ef1a6b233839
natpaw <msnatpaw@gmail.com>
2022-05-18 17:41:41 +0300

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  serve_static_files = true
+  config.serve_static_files = true
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
